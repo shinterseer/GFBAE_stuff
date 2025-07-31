1f3c0d566c6189941e5b87838a322bff3d90a430
Simon Hinterseer <simon.hinterseer@tuwien.ac.at>
2025-07-31 11:03:34 +0200

    refining my numba_test.py

--- numba_test.py
+++ numba_test.py
@@ -1,22 +1,15 @@
 import numpy as np
 
 from numba import jit
-from numba import float64
+from numba import float64, int32
 from numba.types import Array
 from numba.experimental import jitclass
 
 import time
 
 
-def crunching_numbers(workload):
-    local = 0
-    for i in range(workload):
-        local = local + 1
-    return local
-
-
 @jit(nopython=True)
-def crunching_numbers_jit(workload):
+def crunching_numbers(workload):
     local = 0
     for i in range(workload):
         local = local + 1
@@ -49,42 +42,63 @@ class HeatDiffusionClass:
         self.u_init[0] = 1
         self.alpha = alpha
         self.dx = dx
-        self.u_init = np.zeros(size)
 
     def simulate(self, dt, steps):
         u_local = self.u_init.copy()
+        local_alpha = self.alpha # for some reason this makes it like 20-30% faster
+        local_dx = self.dx # for some reason this makes it like 20-30% faster
         for step in range(steps):
             u_new = u_local.copy()
             for i in range(1, len(u_local) - 1):
-                u_new[i] = u_local[i] + self.alpha * dt / self.dx ** 2 * (u_local[i - 1] - 2 * u_local[i] + u_local[i + 1])
+                u_new[i] = u_local[i] + local_alpha * dt / local_dx ** 2 * (u_local[i - 1] - 2 * u_local[i] + u_local[i + 1])
             u_local = u_new
         return u_local
 
 
-def simple_script():
-    print('non jit... ', end='')
+def simple_script(manual_precompilation=True):
+    if manual_precompilation:
+        start = time.time()
+        print('Manually precompiling... ', end='')
+        crunching_numbers.compile((int32,))
+        end = time.time()
+        print(f'{end - start:.4f}')
+
+    workload = int(1.e8)
+    print('pure python... ', end='')
     start = time.time()
-    crunching_numbers(int(1.e8))
+    crunching_numbers.py_func(workload)
     end = time.time()
     print(f'{end - start:.4f}')
 
-    print('jit 1st... ', end='')
+    workload = int32(1.e8)
+    print('numba compiled version...  ', end='')
     start = time.time()
-    crunching_numbers_jit(int(1.e8))
-    end = time.time()
-    print(f'{end - start:.4f}')
-
-    print('jit 2nd... ', end='')
-    start = time.time()
-    crunching_numbers_jit(int(1.e8))
+    crunching_numbers(workload)
     end = time.time()
     print(f'{end - start:.4f}')
 
 
-def heat_diffusion_script():
-    print('running... ', end='')
+def heat_diffusion_script(manual_precompilation=True):
+    # function header:
+    # heat_diffusion(size, dx, dt, alpha, steps)
+    if manual_precompilation:
+        start = time.time()
+        print('Manually precompiling... ', end='')
+        heat_diffusion.compile((int32, float64, float64, float64, int32,))
+        end = time.time()
+        print(f'{end - start:.4f}')
+
+    args = (int(1.e4), 0.1, 0.001, 1., int(1.e4))
+    print('pure python... ', end='')
     start = time.time()
-    heat_diffusion(size=int(1.e4), dx=0.1, dt=0.1, alpha=1, steps=int(1.e4))
+    heat_diffusion.py_func(*args)
+    end = time.time()
+    print(f'{end - start:.4f}')
+
+    args_numba = (int32(1.e4), float64(0.1), float64(0.001), float64(1.), int32(1.e4))
+    print('numba compiled version...  ', end='')
+    start = time.time()
+    heat_diffusion(*args_numba)
     end = time.time()
     print(f'{end - start:.4f}')
 
@@ -95,14 +109,15 @@ def heat_diffusion_class_script():
     print('simulating... ', end='')
     start = time.time()
     # hd.simulate(dt=.1, steps=int(2.e3))
-    hd.simulate(.1, int(2.e3))
+    hd.simulate(float64(.001), int32(1.e4))
     end = time.time()
     print(f'{end - start:.4f}')
 
 
+
 def main():
     heat_diffusion_class_script()
-
+    heat_diffusion_class_script()
 
 if __name__ == '__main__':
     main()
